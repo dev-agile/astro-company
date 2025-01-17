f32815079b9195a7059a8938da27797da790ebe5
Apple <apple@Apples-MacBook-Pro.local>
2025-01-17 13:42:19 +0530

    test 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
 
-// https://astro.build/config
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
